420ede49b3f8cb630d234399255b1dc3ba0db7c2
Lee Randall <lrandall@equinoxx.uk>
2024-05-15 11:52:09 +0100

    Made tutorials dynamic, simply edit the stream.json file

---
+++ TutorialHub/Models/TutorialsModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace TutorialHub.Models
+{
+    public class TutorialsModel
+    {
+        [DataMember(Name = "tutorials")]
+        public Tutorials Tutorials { get; set; }
+    }
+
+    public class Stream
+    {
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+        [DataMember(Name = "iframe")]
+        public string Iframe { get; set; }
+    }
+
+    public class Tutorials
+    {
+        [DataMember(Name = "stream")]
+        public List<Stream> Stream { get; set; }
+    }
+
+}
--- TutorialHub/Pages/Index.cshtml.cs
+++ TutorialHub/Pages/Index.cshtml.cs
@@ -1,17 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using TutorialHub.Models;
 
 namespace TutorialHub.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public TutorialsModel Data
+        {
+            get
+            {
+                string rawJson = System.IO.File.ReadAllText("./Data/stream.json");
+                return JsonConvert.DeserializeObject<TutorialsModel>(rawJson);
+            }
+        }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
         public void OnGet()
         {
 
